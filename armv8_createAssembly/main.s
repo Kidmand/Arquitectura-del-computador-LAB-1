4fb9a712b4bcba3997717c2fda92e5f4f6f706ae
Daián Exequiel Giménez <daian.gimenez@mi.unc.edu.ar>
2024-09-08 18:40:02 -0300

    feat: Se corriegieron algunos de errores en el main.s

--- armv8_createAssembly/main.s
+++ armv8_createAssembly/main.s
@@ -19,7 +19,7 @@ loop:
     CBZ  x9, finloop 
     CBZ  xzr, loop
 
-finloop: CBZ XZR, finloop
+end: CBZ XZR, end		// Bucle infinito
 
 // ------------------- FIN -------------------- //
 
@@ -28,28 +28,24 @@ finloop: CBZ XZR, finloop
 // Realizar la sumatoria de las primeras N posiciones de memoria y guardar el
 // resultado en la posición N+1.
 
-
-    // Definir la constante
-    .equ CONST_N, 10
-
     // Inicialización
-    SUB x0, x0, x0          // Establecer x0 en 0 (base de memoria)
-    SUB x3, x3, x3          // Establecer x3 en 0 (acumulador)
-    ADD x1, x0, CONST_N     // Establecer x1 en N (número de elementos)
+    SUB x10, x10, x10          // Establecer x10 en 0 (base de memoria)
+    SUB x13, x13, x13          // Establecer x13 en 0 (acumulador)
+    ADD x12, x10, x15    		// Establecer x12 en N (número de elementos)
 
-	CBZ x1, End             // Si x1 es 0, saltar a End (resultado final)
+	CBZ x12, End             // Si x12 es 0, saltar a End (resultado final)
 loop:
-    LDUR x2, [x0]            // Cargar el valor desde la dirección base en x2
-    ADD  x3, x3, x2          // Sumar el valor en x2 al acumulador x3
-    ADD  x0, x0, #8          // Avanzar la dirección base en 8 bytes
-    SUB  x1, x1, #1          // Decrementar el contador N
+    LDUR x14, [x10]            // Cargar el valor desde la dirección base en x14
+    ADD  x13, x13, x14          // Sumar el valor en x14 al acumulador x13
+    ADD  x10, x10, #8          // Avanzar la dirección base en 8 bytes
+    SUB  x12, x12, #1          // Decrementar el contador N
     
-    CBZ  x1, end             // Verificar de nuevo si x1 es 0, si es así, saltar a End
-    CBZ  xzr, loop           // Volver al inicio del bucle si x1 no es 0 (xzr siempre es 0, así que esto siempre regresa al bucle)
+    CBZ  x12, end             // Verificar de nuevo si x12 es 0, si es así, saltar a End
+    CBZ  xzr, loop           // Volver al inicio del bucle si x12 no es 0 (xzr siempre es 0, así que esto siempre regresa al bucle)
 
 end:
-    STUR  x3, [x0]            // Almacenar el resultado acumulado en la posición N+1
-
+    STUR  x13, [x10]            // Almacenar el resultado acumulado en la posición N+1
+end1: CBZ XZR, end1			  // Bucle infinito
 
 // ------------------- FIN -------------------- //
 
@@ -61,14 +57,15 @@ end:
     SUB X14, X14, X14      // Inicializar X14 (temporal) a 0 (Acumulador para el resultado)
 	ADD X15, X17, #0       // Inicializar X15 (temporal) a X17 (Para llevar el conteo de iteraciones)
 
-loop:  
 	CBZ X15, end           // Si X15 es 0, saltar a "end"
+loop:  
     ADD X14, X14, X16      // Sumar X16 a X14 (Resultado acumulado)
     SUB X15, X15, #1       // Decrementar el contador
-    B loop                 // Repetir el bucle
+	CBZ  x15, end          // Verificar de nuevo si x15 es 0, si es así, saltar a End
+    CBZ  xzr, loop         // Volver al inicio del bucle si x15 no es 0 (xzr siempre es 0, así que esto siempre regresa al bucle)
 
 end: 
 	SUB X13, X13, X13      // Inicializar X13 (temporal) a 0
     STUR X14, [X13, #0]    // Guardar el resultado en la dirección 0 de memoria
-
+end1: CBZ XZR, end1        // Bucle infinito
 // ------------------- FIN -------------------- //
