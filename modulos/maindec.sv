f8287b68914838cb7b96c78bc3069e296225e333
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-09-08 13:49:54 -0300

    feat: refactor order.

--- modulos/maindec.sv
+++ modulos/maindec.sv
@@ -4,10 +4,10 @@ module maindec(
     output logic [1:0] ALUOp);
 
     logic [8:0] output_signals [0:4] = '{
+        9'b0_0_0_1_0_0_0_10, // R-format
         9'b0_1_1_1_1_0_0_00, // LDUR
         9'b1_1_0_0_0_1_0_00, // STUR
         9'b1_0_0_0_0_0_1_01, // CBZ
-        9'b0_0_0_1_0_0_0_10, // R-format
         9'b0_0_0_0_0_0_0_00  // default
     };
 
@@ -23,17 +23,17 @@ module maindec(
 
     always_comb begin
         casez (Op)
-            // LDUR
-            11'b111_1100_0010: res = output_signals[0];
-            // STUR
-            11'b111_1100_0000: res = output_signals[1];
-            // CBZ
-            11'b101_1010_0???: res = output_signals[2];
             // TYPE R
             11'b100_0101_1000,
             11'b110_0101_1000,
             11'b100_0101_0000,
-            11'b101_0101_0000: res = output_signals[3];
+            11'b101_0101_0000: res = output_signals[0];
+            // LDUR
+            11'b111_1100_0010: res = output_signals[1];
+            // STUR
+            11'b111_1100_0000: res = output_signals[2];
+            // CBZ
+            11'b101_1010_0???: res = output_signals[3];
             default: res = output_signals[4];
         endcase
     end
