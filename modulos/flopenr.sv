33c30501f9238cba05788583ee8fc42bfbe9d2fd
kidmand <daian.gimenez@mi.unc.edu.ar>
2024-10-05 02:20:29 -0300

    Feat: se implementó el registro para almacenar el estado de las banderas.

---
+++ modulos/flopenr.sv
@@ -0,0 +1,15 @@
+module flopenr #(parameter int unsigned N = 64)
+    (input logic clk,
+     input logic reset,
+     input logic enable,        // Agregar la señal de habilitación
+     input logic [N-1:0] d,
+     output logic [N-1:0] q );
+
+    always_ff @(posedge clk, posedge reset) begin
+        if (reset)
+            q <= '0;               // Reiniciar el registro a 0
+        else if (enable)
+            q <= d;               // Actualizar solo si enable es alto
+    end
+
+endmodule
