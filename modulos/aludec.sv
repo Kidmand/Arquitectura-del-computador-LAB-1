373b208f775dc8c93d6585cbabe658d0765e25e5
kidmand <daian.gimenez@mi.unc.edu.ar>
2024-10-05 01:06:25 -0300

    Feat: se implementaron la salida de aluControl para las nuevas instrucciones

--- modulos/aludec.sv
+++ modulos/aludec.sv
@@ -19,6 +19,10 @@ module aludec (input  logic [10:0] funct,
         else if((aluop == 2'b10)  & (funct == 11'b10001010000)) alucontrol = 4'b0000;    //AND
         else if((aluop == 2'b10)  & (funct == 11'b10101010000)) alucontrol = 4'b0001;    //OR
         else if((aluop == 2'b11)  & (funct[10:1] == 10'b1001000100)) alucontrol = 4'b0010; //ADDI
-        else if(( aluop == 2'b11) & (funct[10:1] == 10'b1101000100)) alucontrol = 4'b0110; //SUBI
+        else if((aluop == 2'b11)  & (funct[10:1] == 10'b1101000100)) alucontrol = 4'b0110; //SUBI
+        else if((aluop == 2'b11)  & (funct[10:1] == 10'b1011000100)) alucontrol = 4'b1010; //ADDIS
+        else if((aluop == 2'b11)  & (funct[10:1] == 10'b1111000100)) alucontrol = 4'b1110; //SUBIS
+        else if((aluop == 2'b10)  & (funct == 11'b10101011000)) alucontrol = 4'b1010; //ADDS
+        else if((aluop == 2'b10)  & (funct == 11'b11101011000)) alucontrol = 4'b1110; //SUBS
         else alucontrol = 4'b0000;
 endmodule
