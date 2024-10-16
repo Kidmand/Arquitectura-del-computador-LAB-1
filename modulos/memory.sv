3a30b0778b6f6dba6824d42f4593a4973a99a1ec
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-16 12:47:46 -0300

    fix: se corrigio la salida PCSrc_M

--- modulos/memory.sv
+++ modulos/memory.sv
@@ -4,7 +4,9 @@ module memory     (input logic Branch_M, zero_M, zero_flag, negative, overflow, 
                     input logic [4:0] Rt_B_cond,
                     output logic PCSrc_M);
 
-    bCondCheck Bcond_check(
+    logic B_cond_is;
+
+    bCondCheck B_cond_check(
         .zero(zero_flag),
         .negative(negative),
         .carry(carry),
@@ -13,9 +15,9 @@ module memory     (input logic Branch_M, zero_M, zero_flag, negative, overflow, 
         .Rt_B_cond(Rt_B_cond),
         .B_cond_is(B_cond_is)
     );
-    
-    mux2    MUX2 (.d0(Branch_M), .d1(B_cons_is), .s(bCondCheck), .y(PCSrc_M));
 
-    assign PCSrc_M = Branch_M & zero_M;
+    mux2    MUX2 (.d0(Branch_M & zero_M), .d1(B_cond_is), .s(bCondCheck), .y(PCSrc_M));
+
+    //assign PCSrc_M = Branch_M & zero_M;
 
 endmodule
