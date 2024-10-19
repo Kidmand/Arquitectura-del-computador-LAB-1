5abb6af13c354d3819162703fbaeecd795231de5
Ramiro Lugo <ramiro.lugo@mi.unc.edu.ar>
2024-10-19 20:31:16 -0300

    fix: no se estaba indicando el N del nuevo mux en MEMORY.

--- modulos/memory.sv
+++ modulos/memory.sv
@@ -18,8 +18,6 @@ module memory     (input logic Branch_M, zero_M, zero_flag, negative, overflow, 
     );
 
     assign PCSrc_M_aux_ = Branch_M && zero_M;
-    mux2    MUX2 (.d0(PCSrc_M_aux_), .d1(B_cond_is), .s(bCondCheck), .y(PCSrc_M));
-
-    //assign PCSrc_M = Branch_M & zero_M;
+    mux2 #(1) MUX2 (.d0(PCSrc_M_aux_), .d1(B_cond_is), .s(bCondCheck), .y(PCSrc_M));
 
 endmodule
