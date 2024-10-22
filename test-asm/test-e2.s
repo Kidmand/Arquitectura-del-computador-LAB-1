753410b878308ca69f43de5f3b92c5579c0ab184
vmatiasw <matias.viola@mi.unc.edu.ar>
2024-10-22 09:21:45 -0300

    test: test corregidos y revisados

--- test-asm/test-e2.s
+++ test-asm/test-e2.s
@@ -1,8 +1,8 @@
     .text
     .org 0x0000
     // Los tests pasan si las señales de N, Z, C, V en wave corresponden y
-    // se realizan los saltos sin escribir en memoria su id correspondiente
-    // en caso de fallar en la posicion 0x0
+    // la posicion 0x0 de la memoria contiene 0xFFFFFFFFFFFFFFFF.
+    // Los tests fallan si la posicion 0x0 de la memoria contiene el id del test que fallo.
     //                        
     // caso de    salto signed   B.EQ: Z=1          id = 1
     // caso de no salto signed   B.EQ: Z=1          id = 2
@@ -47,6 +47,8 @@
 // --------------------------------------------------------
     // Test de B.EQ signado // caso de salto
     SUB X29, X1, X2    // X29 = 1-2 = -1
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
     SUBS X28, X29, X29 // X28 = -1-(-1) = 0 => Z=1
 
     B.EQ signed_BEQ_pass
@@ -56,6 +58,8 @@ signed_BEQ_pass:
 // --------------------------------------------------------
     // Test de B.EQ signado // caso de no salto
     SUB X29, X0, X2    // X29 = 0-2 = -2
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
     SUBS X28, X29, X1  // X28 = -2-1 = -3 => Z=0
 
     ADD X30, X0, #2
@@ -63,6 +67,8 @@ signed_BEQ_pass:
 // --------------------------------------------------------
     // Test de B.EQ no signado // caso de salto
     ADD X29, X1, X2    // X29 = 1+2 = 3
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
     SUBS X28, X29, X29 // X28 = 3-3 = 0 => Z=1
 
     B.EQ unsigned_BEQ_pass
@@ -72,19 +78,24 @@ unsigned_BEQ_pass:
 // --------------------------------------------------------
     // Test de B.EQ no signado // caso de no salto
     ADD X29, X1, X1    // X29 = 1+1 = 2
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
     SUBS X28, X29, X1  // X28 = 2-1 = 1 => Z=0
 
     ADD X30, X0, #4
     B.EQ instruction_fail
 
 
-    ADD X30, X0, #0
+    ADD X30, X0, #-1 // X30 = 0xFFFFFFFFFFFFFFFF si pasan todos los test
 // --------------------------------------------------------
-endloop:
-    B endloop         // Bucle infinito
 
 instruction_fail: // ARGs: X30 = id de test
     // Se escribe en memoria el id del test que fallo
     // en la direccion 0X0
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
     STUR X30, [X0, #0] // MEM[X0] = id
-    B endloop
+
+endloop:
+    B endloop         // Bucle infinito
+
