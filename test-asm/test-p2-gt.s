cec5b5918ba35ea29e074f4b2c25a69dc5620783
ramiro-l <ramideporte@gmail.com>
2024-11-01 11:08:20 -0300

    fix: se arreglo el test de gt.

--- test-asm/test-p2-gt.s
+++ test-asm/test-p2-gt.s
@@ -1,7 +1,7 @@
     .text
     .org 0x0000
 
-    // TEST de GT (>=) 
+    // TEST de GT (>) 
 
     // registos reservados
     // X29 = auxiliar
@@ -19,7 +19,7 @@
     ADD x22, x0, #-1 // Inicializa iterador del mensaje de fallo
 // 1 ------------------------------------------------------------------
     // Test de B.GT
-    SUBS X28, X4, X3 // 4 >= 3 (true)
+    SUBS X28, X4, X3 // 4 > 3 (true)
 
     B.GT t1_GT                
         ADD XZR, XZR, XZR
@@ -34,22 +34,28 @@ t1_GT:
     SUB x22, x22, #1
 // 2 ------------------------------------------------------------------
     // Test de B.GT
-    SUBS X28, X4, X4 // 4 >= 4 (true)
+    SUBS X28, X4, X4 // 4 > 4 (false)
 
     B.GT t2_GT                
         ADD XZR, XZR, XZR
         ADD XZR, XZR, XZR
         ADD XZR, XZR, XZR
-    STUR X22, [x20, #0]     // MEM: 0xFFFFFFFFFFFFFFFE (si falla) else 0x0
-t2_GT:
     STUR X21, [x20, #8]     // MEM: 0x2
+    CBZ XZR, t2_GT_end
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
+t2_GT:
+    STUR X22, [x20, #0]     // MEM: 0xFFFFFFFFFFFFFFFE (si falla) else 0x0
+t2_GT_end:
+
 // --------------------------------------------------------------------
     ADD x20, x20, #16 // i += 16
     ADD x21, x21, #1
     SUB x22, x22, #1
 // 3 ------------------------------------------------------------------
     // Test de B.GT
-    SUBS X28, X4, #3 // 4 >= 3 (true)
+    SUBS X28, X4, #3 // 4 > 3 (true)
 
     B.GT t3_GT                
         ADD XZR, XZR, XZR
@@ -64,22 +70,27 @@ t3_GT:
     SUB x22, x22, #1
 // 4 ------------------------------------------------------------------
     // Test de B.GT
-    SUBS X28, X4, #4 // 4 >= 4 (true)
+    SUBS X28, X4, #4 // 4 > 4 (false)
 
     B.GT t4_GT                
         ADD XZR, XZR, XZR
         ADD XZR, XZR, XZR
         ADD XZR, XZR, XZR
-    STUR X22, [x20, #0]     // MEM: 0xFFFFFFFFFFFFFFFC (si falla) else 0x0
-t4_GT:
     STUR X21, [x20, #8]     // MEM: 0x4
+    CBZ XZR, t4_GT_end
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
+        ADD XZR, XZR, XZR
+t4_GT:
+    STUR X22, [x20, #0]     // MEM: 0xFFFFFFFFFFFFFFFC (si falla) else 0x0
+t4_GT_end:
 // --------------------------------------------------------------------
     ADD x20, x20, #16 // i += 16
     ADD x21, x21, #1
     SUB x22, x22, #1
 // 5 --------------------------------------------------------------------
     // Test de B.GT
-    SUBS X28, X2, X3 // 2 >= 3 (false)
+    SUBS X28, X2, X3 // 2 > 3 (false)
 
     B.GT t5_GT                
         ADD XZR, XZR, XZR
@@ -99,7 +110,7 @@ t5_GT_end:
     SUB x22, x22, #1
 // 6 --------------------------------------------------------------------
     // Test de B.GT
-    SUBS X28, X2, #3 // 2 >= 3 (false)
+    SUBS X28, X2, #3 // 2 > 3 (false)
 
     B.GT t6_GT                
         ADD XZR, XZR, XZR
