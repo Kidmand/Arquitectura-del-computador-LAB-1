deb190c1d846e84095e7428a5894febfea9e04c1
daian.gimenez@mi.unc.edu.ar <daian.gimenez@mi.unc.edu.ar>
2024-11-10 14:20:37 -0300

    feat: se mejoró el script de run.sh

--- test-asm/run.sh
+++ test-asm/run.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usa: ./run.sh <test-file>"
-    exit 1
-fi
+# Define una lista de archivos con sus direcciones completas
+FILES=(
+    "test-p1-1.s"
+    "test-p1-2.s"
+    "test-p2-eq-ne-lo-mi-hs.s"
+    "test-p2-eq.s"
+    "test-p2-eq.s"
+    "test-p2-ge.s"
+    "test-p2-gt.s" 
+    "test-p2-hi.s" 
+    "test-p2-le.s" 
+    "test-p2-lt.s" 
+    "test-p2-ls.s" 
+    "test-p2-pl.s" 
+    "test-p2-vc.s" 
+    "test-p2-vs.s"
+)
 
-TEST_FILE=$1
-FILE_NAME=$(basename "$1" .s)
+for TEST_FILE in "${FILES[@]}"; do
+    echo "Ejecutando para archivo: $TEST_FILE"
+    echo "..............................................."
 
-echo "..............................................."
+    ./compile.sh "$TEST_FILE" -i
 
-./compile.sh "$TEST_FILE" -i
+    echo "..............................................."
 
-echo "..............................................."
+    # Ejecuta la simulación RTL
+    echo "Corriendo la simulación RTL para el proyecto ..."
+    ./quartus-cli.sh --rtl --no-gui >/dev/null
 
-echo "Corriendo la simulación RTL para el proyecto ..."
-./quartus-cli.sh --rtl --no-gui >/dev/null
+    echo "..............................................."
 
-echo "..............................................."
+    # Agrega la salida de memdump al archivo de prueba .s, entre /* y */
+    echo "/*" >> "$TEST_FILE"
+    echo "Salida en memdump:" >> "$TEST_FILE"
+    ./get_memdump.sh >> "$TEST_FILE"
+    echo "Finalizado para archivo: $TEST_FILE" >> "$TEST_FILE"
+    echo "===============================================" >> "$TEST_FILE"
+    echo "*/" >> "$TEST_FILE"
 
-echo "Salida en memdump:"
-./get_memdump.sh
+    echo "Finalizado para archivo: $TEST_FILE"
+    echo "Salida agregada en $TEST_FILE"
+    echo "==============================================="
+done
